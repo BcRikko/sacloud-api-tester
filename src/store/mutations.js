7551074451a09838f694d9e22fc237176a05ec67
BcRikko <s.shin1106@gmail.com>
2016-10-21 18:09:45 +0900

    API実行時にstate.historyに登録するように変更

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,6 +4,16 @@ export default {
   [types.API_REQUEST] (state, result) {
     const response = JSON.stringify(result, null, '  ')
     state.response = response
+
+    state.history.push({
+      request: {
+        zone: state.request.zone,
+        method: state.request.method,
+        uri: state.request.uri,
+        params: state.request.params
+      },
+      response: state.response
+    })
   },
   [types.SELECT_API] (state, api) {
     state.request.method = api.method
